d78d930288524d24cd3b98cfbf93c7b3aaab4d9c
London <london.rosemary@gmail.com>
2021-08-24 11:22:14 -0400

    Add background image

--- src/index.js
+++ src/index.js
@@ -1,26 +1,49 @@
-let baseUrlForMeal = 'http://www.themealdb.com/api/json/v1/1/random.php/${meal}'
+const BASE_URL_MEAL = 'www.themealdb.com/api/json/v1/1/random.php'
 
-const generateFoodButton = document.querySelector(`#meal-button`)
-
-//Add event listener click for Generate button: 
-generateFoodButton.addEventListener('click', event => {
-    console.log('click',)
-    fetch(baseUrlForMeal, {
-        mode: 'no-cors',
-        credentials: 'include',
-    })
-        .then(function (response) {
-        console.log(response);
-        return response.json();
+function generateRandomMeal() {
+    const mealButton = document.querySelector("#meal-button")
+    mealButton.addEventListener('click', e => {
+        fetch(BASE_URL_MEAL, {
+            mode: 'no-cors',
+            credentials: 'include',
         })
-        .then(data => console.log(data))
-        //.then(resp => resp.json())
-        // .then(response => response.json())
-        //    ramenArr.forEach(ramenObject => {
-        //  function renderRamenImg (ramenObject){
+        .then(data => data.json())
+        .then(resp => console.log(resp))
+    })
+}
 
-        //     }
-            // })
+function init() {
+    generateRandomMeal()
+}
 
-})
+init()
+
+
+
+
+// let baseUrlForMeal = 'http://www.themealdb.com/api/json/v1/1/random.php/${meal}'
+
+// const generateFoodButton = document.querySelector(`#meal-button`)
+
+// //Add event listener click for Generate button: 
+// generateFoodButton.addEventListener('click', event => {
+//     console.log('click',)
+//     fetch(baseUrlForMeal, {
+//         mode: 'no-cors',
+//         credentials: 'include',
+//     })
+//         .then(function (response) {
+//         console.log(response);
+//         return response.json();
+//         })
+//         .then(data => console.log(data))
+//         //.then(resp => resp.json())
+//         // .then(response => response.json())
+//         //    ramenArr.forEach(ramenObject => {
+//         //  function renderRamenImg (ramenObject){
+
+//         //     }
+//             // })
+
+// })
 
